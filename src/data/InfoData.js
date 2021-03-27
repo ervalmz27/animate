b1a141525d375ac41e0fe8af3d200a710bd6ab00
naufal <naufalmtz27@gmail.com>
2021-03-27 13:33:45 +0700

    first commit

---
+++ src/data/InfoData.js
@@ -0,0 +1,24 @@
+import ImageOne from '../images/kurma.jpg';
+import ImageTwo from '../images/motion.jpg';
+
+
+
+export const InfoData = {
+    heading: 'Explore our beatiful homes',
+    paragraphOne: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor mollitia aliquam aliquid blanditiis saepe vitae necessitatibus sunt iste enim delectus? Fuga placeat consectetur adipisci impedit veritatis hic, in quae corrupti.',
+    paragraphTwo: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor mollitia aliquam aliquid blanditiis saepe vitae necessitatibus sunt iste enim delectus? Fuga placeat consectetur adipisci impedit veritatis hic, in quae corrupti. ',
+    buttonLabel: 'View Homes',
+    image: ImageOne,
+    reverse: false,
+    delay: 100
+};
+export const InfoDataTwo= {
+    heading: 'Modern Design',
+    paragraphOne: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor mollitia aliquam aliquid blanditiis saepe vitae necessitatibus sunt iste enim delectus? Fuga placeat consectetur adipisci impedit veritatis hic, in quae corrupti.',
+    paragraphTwo: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor mollitia aliquam aliquid blanditiis saepe vitae necessitatibus sunt iste enim delectus? Fuga placeat consectetur adipisci impedit veritatis hic, in quae corrupti. ',
+    buttonLabel: 'View Homes',
+    image: ImageTwo,
+    reverse: true,
+    delay: 300
+};
+     
---
+++ src/data/SliderData.js
@@ -0,0 +1,74 @@
+import ImageOne from '../images/image2.jpg';
+import ImageTwo from '../images/hacok.jpg';
+import ImageThree from '../images/image3.jpg';
+import ImageFour from '../images/image4.jpg';
+
+
+export const SliderData = [
+    {
+        title: 'Luxury Vila in Bali,Indonesia',
+        priece: '$4.200.000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageOne,
+        alt: 'House'
+    },
+    {
+        title: 'House in CalaBasas,California',
+        priece: '$8.200.000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageTwo,
+        alt: 'House'
+    },
+    {
+        title: 'House in Houston,Texas',
+        priece: '$4.200.000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageThree,
+        alt: 'House'
+    },
+    {
+        title: 'Luxury Vila in Bali,Indonesia',
+        priece: '$4.200.000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageFour,
+        alt: 'House'
+    },
+];
+// export const SliderDataTwo = [
+//     {
+//         title: 'Luxury Vila in Bali,Indonesia',
+//         Priece: '$4.200.000',
+//         path: '/homes',
+//         label: 'View Home',
+//         image: ImageOne,
+//         alt: 'House'
+//     },
+//     {
+//         title: 'Luxury Vila in Bali,Indonesia',
+//         Priece: '$4.200.000',
+//         path: '/homes',
+//         label: 'View Home',
+//         image: ImageTwo,
+//         alt: 'House'
+//     },
+//     {
+//         title: 'Luxury Vila in Bali,Indonesia',
+//         Priece: '$4.200.000',
+//         path: '/homes',
+//         label: 'View Home',
+//         image: ImageThree,
+//         alt: 'House'
+//     },
+//     {
+//         title: 'Luxury Vila in Bali,Indonesia',
+//         Priece: '$4.200.000',
+//         path: '/homes',
+//         label: 'View Home',
+//         image: ImageFour,
+//         alt: 'House'
+//     },
+// ];
